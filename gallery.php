4b0d1d60c417653141c5ca5043433b7ebbbe7b33
shubham sutar <68117385+shubham-sutar@users.noreply.github.com>
2022-02-09 18:44:47 +0530

    Added All Files.

---
+++ gallery.php
@@ -0,0 +1,185 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>YK Photos</title>
+        <meta name = "viewport" content = "width=device-width, intial-scale=1.0">
+        <link rel="stylesheet" href="main.css">
+        <script src="https://kit.fontawesome.com/dbed6b6114.js" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        
+        <!-- header -->
+        <header class = "header">
+            <div class = "nav-section">
+                <div class = "brand-and-navBtn">
+                    <span class = "brand-name">
+                        YK Photofilms
+                    </span>
+                    <span class = "navBtn flex">
+                        <i class = "fas fa-bars"></i>
+                    </span>
+                </div>
+
+                <!-- navigation menu -->
+                <nav class = "top-nav">
+                    <ul>
+                        <li><a href = "index.html">Home</a></li>
+                        <li><a href = "gallery.html" class = "active">Gallery</a></li>
+                        <li><a href = "contact.html">Contact</a></li>
+                    </ul>
+                </nav>
+                <span class = "search-icon">
+                    <i class = "fas fa-search"></i>
+                </span>
+            </div>
+
+            <div class = "container about">
+                <div class = "about-content">
+                    <div class = "about-img flex">
+                        <img src = "image/about-img.jpg" alt = "photographer img">
+                    </div>
+                    <h2>I'm Yogesh Korabu</h2>
+                    <h3><b>Photographer | Traveller</b></h3>
+                    <blockquote>
+                        "Photography is a way of feeling, of touching, of loving. What you have caught on film is captured forever ... It remembers little things, long after you have forgotten everything."
+                        <span>-Aaron Siskind</span>
+                    </blockquote>
+                </div>
+
+                <div class = "social-icons">
+                    <ul>
+                        <li>
+                            <a href = "https://www.facebook.com/yogesh.korabu" target="_blank"><i class = "fab fa-facebook"></i></a>
+                        </li>
+                        <li>
+                            <a href = "#"><i class = "fab fa-twitter"></i></a>
+                        </li>
+                        <li>
+                            <a href="https://www.instagram.com/yogesh_korabu_photofilms/" target="_blank"><i class="fab fa-instagram"></i></a>
+                        </li>
+                        <li>
+                            <a href="https://www.youtube.com/channel/UCKEn4J-fb5MvkkTQn6Saf2Q" target="_blank"><i class="fab fa-youtube"></i></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </header>
+        <!-- end of header -->
+
+        <!-- main -->
+        <section class = "section-three">
+            <div class = "container">
+                <div class = "gallery">
+                    <a href = "image/gallery1.jpg">
+                        <div class = "single-img">
+                            <img src = "image/gallery1.jpg" alt = "gallery image">
+                            <div class = "single-img-content">
+                                <h3>Model Shoot</h3>
+                            </div>
+                        </div>
+                    </a>
+
+                    <a href = "image/gallery2.jpg">
+                        <div class = "single-img">
+                            <img src = "image/gallery2.jpg" alt = "gallery image">
+                            <div class = "single-img-content">
+                                <h3>Food Photography</h3>
+                            </div>
+                        </div>
+                    </a>
+
+                    <a href = "image/gallery3.jpg">
+                        <div class = "single-img">
+                            <img src = "image/gallery3.jpg" alt = "gallery image">
+                            <div class = "single-img-content">
+                                <h3>Art Work</h3>
+                            </div>
+                        </div>
+                    </a>
+
+                    <a href = "image/gallery4.jpg">
+                        <div class = "single-img">
+                            <img src = "image/gallery4.jpg" alt = "gallery image">
+                            <div class = "single-img-content">
+                                <h3>Chiled Photography</h3>
+                            </div>
+                        </div>
+                    </a>
+
+                    <a href = "image/gallery5.jpg">
+                        <div class = "single-img">
+                            <img src = "image/gallery5.jpg" alt = "gallery image">
+                            <div class = "single-img-content">
+                                <h3>Beautiful Work</h3>
+                            </div>
+                        </div>
+                    </a>
+
+                    <a href = "image/gallery6.jpg">
+                        <div class = "single-img">
+                            <img src = "image/gallery6.jpg" alt = "gallery image">
+                            <div class = "single-img-content">
+                                <h3>Couple Shoot</h3>
+                            </div>
+                        </div>
+                    </a>
+
+                    <a href = "image/gallery7.jpg">
+                        <div class = "single-img">
+                            <img src = "image/gallery7.jpg" alt = "gallery image">
+                            <div class = "single-img-content">
+                                <h3>Beautiful Work</h3>
+                            </div>
+                        </div>
+                    </a>
+
+                    <a href = "image/gallery8.jpg">
+                        <div class = "single-img">
+                            <img src = "image/gallery8.jpg" alt = "gallery image">
+                            <div class = "single-img-content">
+                                <h3>Sports Shoot</h3>
+                            </div>
+                        </div>
+                    </a>
+
+                    <a href = "image/gallery9.jpg">
+                        <div class = "single-img">
+                            <img src = "image/gallery9.jpg" alt = "gallery image">
+                            <div class = "single-img-content">
+                                <h3>CanDid Shoot</h3>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </section>
+        <!-- end of main -->
+
+
+        <!-- footer -->
+        <footer class = "footer">
+            <div class = "footer-container container">
+                <div>
+                    <h2>Yogesh Korabu</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel obcaecati sapiente minima itaque nam modi libero optio vitae animi, praesentium quis commodi fugit quasi nostrum nihil veniam nisi suscipit cumque ducimus error doloribus est voluptatum at? Et aut mollitia quia.</p>
+                </div>
+                <div>
+                    <h3>Free Subscription !</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, facere eius! Ipsam laborum modi, cumque dolor incidunt animi nam magni.</p>
+
+                    <div>
+                        <script src="https://apis.google.com/js/platform.js"></script>
+    
+                    <div class="g-ytsubscribe" data-channelid="UCKEn4J-fb5MvkkTQn6Saf2Q" data-layout="default"
+                        data-count="default"></div>
+                    </div>
+                </div>
+            </div>
+            <p>&copy; Designed By Shubham Sutar ❤ Powered By Base64</p>
+        </footer>
+        <!-- end of footer -->
+        
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,173 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>YK Photos</title>
+    <meta name="viewport" content="width=device-width, intial-scale=1.0">
+    <link rel="stylesheet" href="main.css">
+    <script src="https://kit.fontawesome.com/dbed6b6114.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+
+    <!-- header -->
+    <header class="header">
+        <div class="nav-section">
+            <div class="brand-and-navBtn">
+                <span class="brand-name">
+                    YK Photofilm's
+                </span>
+                <span class="navBtn flex">
+                    <i class="fas fa-bars"></i>
+                </span>
+            </div>
+
+            <!-- navigation menu -->
+            <nav class="top-nav">
+                <ul>
+                    <li><a href="index.html" class="active">Home</a></li>
+                    <li><a href="gallery.html">Gallery</a></li>
+                    <li><a href="contact.html">Contact</a></li>
+                </ul>
+            </nav>
+            <span class="search-icon">
+                <i class="fas fa-search"></i>
+            </span>
+        </div>
+
+        <div class="container about">
+            <div class="about-content">
+                <div class="about-img flex">
+                    <img src="image/about-img.jpg" alt="photographer img">
+                </div>
+                <h2>I'm Yogesh Korabu.</h2>
+                <h3><b>Photographer | Traveller</b></h3>
+                <blockquote>
+                    "Photography is a way of feeling, of touching, of loving. What you have caught on film is captured
+                    forever ... It remembers little things, long after you have forgotten everything."
+                    <span>-Aaron Siskind</span>
+                </blockquote>
+            </div>
+
+            <div class="social-icons">
+                <ul>
+                    <li>
+                        <a href="https://www.facebook.com/yogesh.korabu" target="_blank"><i class="fab fa-facebook"></i></a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="fab fa-twitter"></i></a>
+                    </li>
+                    <li>
+                        <a href="https://www.instagram.com/yogesh_korabu_photofilms/" target="_blank"><i class="fab fa-instagram"></i></a>
+                    </li>
+                    <li>
+                        <a href="https://www.youtube.com/channel/UCKEn4J-fb5MvkkTQn6Saf2Q" target="_blank"><i class="fab fa-youtube"></i></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </header>
+    <!-- end of header -->
+
+    <!-- main -->
+    <section class="section-one">
+        <div class="container">
+            <div class="sec-one-left">
+                <!-- image here -->
+                <div>
+                    <h3 style="text-shadow: 2px;">With my camera, I capture daily life</h3>
+                    <p>“Photography is a way of feeling, of touching, of loving. What you have caught on film is captured forever… It remembers little things, long after you have forgotten everything.”</p>
+                    <a href="contact.html" class="btn">view all</a>
+                </div>
+            </div>
+
+            <div class="sec-one-right">
+                <div class="work-content">
+                    <h3>AMAZING TEAM WORK WITH PROFESSIONAL PHOTOGRAPHER</h3>
+                    <p>Over the last few years working in professional portrait photography I have learnt that the success of my endeavours is not solely down to me. It is all about teamwork: ‘a co-ordinated effort on the part of a group of persons acting together as a team in the interests of a common cause’.</p>
+                    <a href="gallery.html" class="btn">view all</a>
+                </div>
+                <div class="work-imgs">
+                    <div class="work-img-1">
+                        <!--image here-->
+                    </div>
+                    <div class="work-img-2">
+                        <!-- image here -->
+                    </div>
+                </div>
+                <h3>"Taking an image, freezing a moment, reveals how rich reality truly is."</h3>
+            </div>
+        </div>
+    </section>
+
+    <section class="section-two">
+        <div class="container">
+            <h2>FOLLOW ON INSTAGRAM</h2>
+            <span>@yogesh_korabu_photofilms</span>
+            <div class="insta-imgs">
+                <div>
+                    <img src="image/insta1.jpg">
+                    <div class="icon-overlay flex">
+                        <i class="fab fa-instagram"></i>
+                    </div>
+                </div>
+                <div>
+                    <img src="image/insta2.jpg">
+                    <div class="icon-overlay flex">
+                        <i class="fab fa-instagram"></i>
+                    </div>
+                </div>
+                <div>
+                    <img src="image/insta3.jpg">
+                    <div class="icon-overlay flex">
+                        <i class="fab fa-instagram"></i>
+                    </div>
+                </div>
+                <div>
+                    <img src="image/insta4.jpg">
+                    <div class="icon-overlay flex">
+                        <i class="fab fa-instagram"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- end of main -->
+
+
+    <!-- footer -->
+    <footer class="footer">
+        <div class="footer-container container">
+            <div>
+                <h2>Yogesh Korabu</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel obcaecati sapiente minima itaque nam
+                    modi libero optio vitae animi, praesentium quis commodi fugit quasi nostrum nihil veniam nisi
+                    suscipit cumque ducimus error doloribus est voluptatum at? Et aut mollitia quia.</p>
+            </div>
+            <div>
+                <h3>Free Subscription !</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, facere eius! Ipsam laborum modi,
+                    cumque dolor incidunt animi nam magni.</p>
+
+                <div>
+                    <script src="https://apis.google.com/js/platform.js"></script>
+
+                <div class="g-ytsubscribe" data-channelid="UCKEn4J-fb5MvkkTQn6Saf2Q" data-layout="default"
+                    data-count="default"></div>
+                </div>
+
+                
+
+            </div>
+        </div>
+        <p>&copy; Designed By Shubham Sutar ❤ Powered By Base64</p>
+    </footer>
+    <!-- end of footer -->
+
+
+    <script src="script.js"></script>
+</body>
+
+</html>
