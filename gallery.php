becb7aef0c36e83b2725b7a591a0ff327867fd7a
shubham sutar <68117385+shubham-sutar@users.noreply.github.com>
2022-02-09 19:02:16 +0530

    Upadate gallery.php

--- gallery.php
+++ gallery.php
@@ -25,9 +25,9 @@
                 <!-- navigation menu -->
                 <nav class = "top-nav">
                     <ul>
-                        <li><a href = "index.html">Home</a></li>
-                        <li><a href = "gallery.html" class = "active">Gallery</a></li>
-                        <li><a href = "contact.html">Contact</a></li>
+                        <li><a href = "index.php">Home</a></li>
+                        <li><a href = "gallery.php" class = "active">Gallery</a></li>
+                        <li><a href = "contact.php">Contact</a></li>
                     </ul>
                 </nav>
                 <span class = "search-icon">
